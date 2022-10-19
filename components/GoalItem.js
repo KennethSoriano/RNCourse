e6b82056199b0496570d1c090dccd5c0e0d946cc
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-10-19 13:49:12 -0700

    styling button

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,11 +3,15 @@ import { StyleSheet, View, Text, Pressable } from 'react-native'
 
 function goalItem(props) {
     return (
-        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-            <View style={styles.goalItem}>
+        <View style={styles.goalItem}>
+            <Pressable 
+                android_ripple={{color: '#dddddd'}} 
+                onPress={props.onDeleteItem.bind(this, props.id)}
+                style={({pressed}) => pressed && styles.pressedItem}
+            >
                 <Text style={styles.goalText}>{props.text}</Text>
-            </View>
-        </Pressable>
+            </Pressable>
+        </View>   
     )
 };
 
@@ -16,12 +20,15 @@ export default goalItem;
 const styles = StyleSheet.create({
     goalItem: {
         margin: 8,
-        padding: 8,
         borderRadius: 6,
         backgroundColor: '#5e0aCC',
         
     },
+    pressedItem: {
+        opacity: 0.5
+    },
     goalText: {
-        color: 'white'
+        color: 'white',
+        padding: 8,
     }
 });
