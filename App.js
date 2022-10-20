75849e5c756a7526fd4d371726e77580f1efb5d3
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-10-20 09:17:00 -0700

    app reset

--- App.js
+++ App.js
@@ -2,60 +2,17 @@ import { StyleSheet, View, Button, FlatList } from 'react-native';
 import GoalItem from './components/GoalItem';
 import { useState } from 'react';
 import GoalInput from './components/GoalInput';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
-  const [modalIsVisible, setModalIsVisible] = useState(false);
-  const [courseGoals, setCourseGoals] = useState([]);
-
-  function startAddGoalHandler() {
-    setModalIsVisible(true);
-  }
-
-
-  function addGoalHandler(enteredGoalText) {
-    setCourseGoals(currentCourseGoals => [
-      ...currentCourseGoals, 
-      {text: enteredGoalText, key: Math.random().toString()},
-    ]);
-  };
-
-  function deleteGoalHandler(id) {
-    setCourseGoals((currentCourseGoals) => {
-      return currentCourseGoals.filter((goal) => {
-        goal.id !== id
-      })
-    })
-  }
 
   return (
-    <View style={styles.appContainer}>
-      <Button 
-        title='Add New Goal' 
-        color='$5e0acc'
-        onPress={startAddGoalHandler}
-      />
-      <GoalInput onAddGoal={addGoalHandler} visible={modalIsVisible} />
-      <View style={styles.goalsContainer}>
-        <FlatList 
-          data={courseGoals} 
-          renderItem={(itemData) => {
-           return (
-           <GoalItem 
-            text={itemData.item.text} 
-            id={itemData.item.id}
-            onDeleteItem={deleteGoalHandler} 
-            />
-           )
-        }} 
-        keyExtractor={(item, index) => {
-          return item.id;
-        }}
-        alwaysBounceVertical={false}>
-
-        </FlatList>
-      </View>
+    <>
+      <View>
         
-    </View>
+      </View>
+    </>
+    
   );
 }
 
@@ -63,7 +20,8 @@ const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
     paddingTop: 50,
-    paddingHorizontal:16
+    paddingHorizontal:16,
+    
   },
   goalsContainer: {
     flex: 5
